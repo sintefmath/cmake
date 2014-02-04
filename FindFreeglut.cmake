8ba08850b3b423e1ea50c11cc80ef45ae43539c0
André R. Brodtkorb <Andre.Brodtkorb@sintef.no>
2014-02-04 11:17:14 +0100

    Added initial version of scripts from svn

---
+++ FindFreeglut.cmake
@@ -0,0 +1,42 @@
+#Tries to find the Freeglut library
+#
+# Copyright SINTEF
+# Author: <Erik W. Bjønnes> Erik.Bjonnes@sintef.no
+
+#find freeglut library
+FIND_LIBRARY(Freeglut_LIBRARY NAMES glut freeglut
+  PATHS
+  #/usr/lib
+  #/usr/lib64
+  ~/mylibs/freeglut/lib
+  /usr/local/lib
+  /usr/local/lib64
+  "C:/scene_cxx_thirdparty/freeglut-2.6.0-3/lib"
+)
+
+#FIXME if glut and freeglut both is installed, it can use glut's library with 
+#freeglut's header. Need to make sure this doesn't happen
+#WORKAROUND, currently only checks in /usr/local/lib so install freeglut there
+
+#find freeglut header
+FIND_PATH(Freeglut_INCLUDE_DIR NAMES GL/freeglut.h
+  PATHS
+  /usr/include
+  /usr/local/include
+  ~/mylibs/freeglut/include
+  "C:/scene_cxx_thirdparty/freeglut-2.6.0-3/include"
+)
+
+#check that we've found it
+SET(Freeglut_FOUND "NO")
+IF(Freeglut_LIBRARY)
+  IF(Freeglut_INCLUDE_DIR)
+    SET(Freeglut_FOUND "YES")
+  ENDIF(Freeglut_INCLUDE_DIR)
+ENDIF(Freeglut_LIBRARY)
+
+#mark freeglut options as advanced
+MARK_AS_ADVANCED(
+  Freeglut_LIBRARY
+  Freeglut_INLUDE_DIR
+)
