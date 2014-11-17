b147e9f771e93f5a69c0b2e99359e9017f36ac82
ErikWB <erik.bjonnes@sintef.no>
2014-11-17 18:01:44 +0100

    Now should work on all supported platforms.
    Rewritten to use find_library, and find both optimised and debug versions.
    Hides as advanced individual libraries, only showing include dir and all libraries.

--- FindGoTools.cmake
+++ FindGoTools.cmake
@@ -3,17 +3,18 @@
 # Original version by: jan.b.thomassen@sintef.no
 # Contributions from sverre.briseid@sintef.no
 # Rewritten to model FindBoost by andre.brodtkorb@sintef.no
+# Rewritten to find optimised&debug by erik.bjonnes@sintef.no
 #
 
 message("Trying to find the following GoTools components: '${GoTools_FIND_COMPONENTS}'")
 
-SET(GOTOOLS_ROOT "" CACHE PATH "Root to GoTools directory")
-MARK_AS_ADVANCED( GOTOOLS_ROOT )
+SET(GoTools_ROOT "" CACHE PATH "Root to GoTools directory")
+MARK_AS_ADVANCED( GoTools_ROOT )
 
 #Set default search paths for includes
 set(GoTools_INCLUDE_SEARCH_PATHS "")
 list(APPEND GoTools_INCLUDE_SEARCH_PATHS
-  "${GOTOOLS_ROOT}/include"
+  "${GoTools_ROOT}/include"
   "$ENV{HOME}/include"
   "$ENV{HOME}/install/include"
   "C:/Program Files (x86)/GoTools/include"
@@ -23,7 +24,7 @@ list(APPEND GoTools_INCLUDE_SEARCH_PATHS
 #Set default search paths for libs
 set(GoTools_LIBRARY_SEARCH_PATHS "")
 list(APPEND GoTools_LIBRARY_SEARCH_PATHS
-  "${GOTOOLS_ROOT}/lib"
+  "${GoTools_ROOT}/lib"
   "$ENV{HOME}/lib"
   "$ENV{HOME}/install/lib"
   "C:/Program Files (x86)/GoTools/lib"
@@ -37,34 +38,36 @@ FIND_PATH(GoTools_INCLUDE_DIRS
   PATH_SUFFIXES GoTools 
 )
 
-SET(GoToolsCoreLibName "")
-IF(WIN32)
-  SET(GoToolsCoreLibName "GoToolsCore.lib")
-ELSE(WIN32)
-  SET(GoToolsCoreLibName "GoToolsCore.so" "GoToolsCore.a")
-ENDIF(WIN32)
 
-# Find library path
-FIND_PATH(GoTools_LIBRARY_DIR
-  NAMES ${GoToolsCoreLibName}
-  PATHS ${GoTools_LIBRARY_SEARCH_PATHS}
-  PATH_SUFFIXES GoTools Win32/Release Win32/Debug Release Debug
-  )
-
-set(GoTools_LIBRARIES "")
+set(GoTools_LIBRARIES_DEBUG "")
+set(GoTools_LIBRARIES_RELEASE "")
 
 foreach(component ${GoTools_FIND_COMPONENTS})
-	message("Finding '${component}'")
-	FIND_LIBRARY(debug_lib
-	  NAMES ${component}
-	  PATHS ${GoTools_LIBRARY_DIR}
-	)
-	FIND_LIBRARY(GoTools_${component}_LIBRARY
-	  NAMES ${component}
-	  PATHS ${GoTools_LIBRARY_DIR}
-	)
-	list(APPEND GoTools_LIBRARIES "${GoTools_${component}_LIBRARY}")
-	message("Gotools libs: ${GoTools_LIBRARIES}")
+  message("Finding '${component}'")
+  FIND_LIBRARY(GoTools_${component}_LIBRARY_DEBUG
+    NAMES ${component}
+    PATHS ${GoTools_LIBRARY_SEARCH_PATHS}
+    PATH_SUFFIXES GoTools Debug Win32/Debug 
+    )
+  list(APPEND GoTools_LIBRARIES_DEBUG "${GoTools_${component}_LIBRARY_DEBUG}")
+
+  FIND_LIBRARY(GoTools_${component}_LIBRARY_RELEASE
+    NAMES ${component}
+    PATHS ${GoTools_LIBRARY_SEARCH_PATHS}
+    PATH_SUFFIXES GoTools Release Win32/Release
+    )
+  list(APPEND GoTools_LIBRARIES_RELEASE "${GoTools_${component}_LIBRARY_RELEASE}")
+  MARK_AS_ADVANCED( GoTools_${component}_LIBRARY_DEBUG )
+  MARK_AS_ADVANCED( GoTools_${component}_LIBRARY_RELEASE )
+
+endforeach()
+
+set( GoTools_LIBRARIES "" )
+foreach( _libname IN LISTS GoTools_LIBRARIES_RELEASE )
+  list( APPEND GoTools_LIBRARIES optimized "${_libname}" )
+endforeach()
+foreach( _libname IN LISTS GoTools_LIBRARIES_DEBUG )
+  list( APPEND GoTools_LIBRARIES debug "${_libname}" )
 endforeach()
 
 
@@ -72,5 +75,11 @@ endforeach()
 SET(GoTools_FOUND FALSE)
 IF(GoTools_INCLUDE_DIRS AND GoTools_LIBRARIES)
   SET(GoTools_FOUND TRUE)
-  SET(GoTools_INCLUDE_DIR ${GoTools_INCLUDE_DIRS})
+  #show the include directory and libraries found
+  SET(GoTools_INCLUDE_DIR ${GoTools_INCLUDE_DIRS} CACHE STRING "GoTools Libraries requested")
+  SET(GoTools_LIBRARY ${GoTools_LIBRARIES} CACHE STRING "GoTools Libraries requested")
+  #hide the specific lists used to set the above variables
+  SET(GoTools_INCLUDE_DIRS CACHE INTERNAL "There are no dependencies, so this should not be used")
+  SET(GoTools_LIBRARIES CACHE INTERNAL "There are no dependencies, so this should not be used")
 ENDIF(GoTools_INCLUDE_DIRS AND GoTools_LIBRARIES)
+
